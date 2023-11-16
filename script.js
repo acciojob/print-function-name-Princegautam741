eb49ee4d6fc6d09f7bb69235c7a4d7f13fec80d3
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-11-16 20:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Define the JavaScript function named functionName
 function functionName() {
-  alert("functionName");
+    alert("functionName");
 }
 
 // Call the function to display its own name
