211025cddb262f5b016ec3fa9894b33ad8c268b9
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-11 17:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Define the function 
 function functionName() {
-
-  // Use arguments.callee.name to print function name
-  alert(arguments.callee.name);
-
+    // Use arguments.callee.name to print function name
+    alert(arguments.callee.name);
 }
 
 // Call the function
